def4507e167dd0e7f353fdc6457e157c1b7042b8
Alumno <73141051@continental.edu.pe>
2023-05-30 16:58:42 -0500

    commit avance lista autos22

--- app/src/main/java/pe/eval2/a10_mplauto/utilidades/Util.java
+++ app/src/main/java/pe/eval2/a10_mplauto/utilidades/Util.java
@@ -1,6 +1,6 @@
 package pe.eval2.a10_mplauto.utilidades;
 
 public class Util {
-    public static final String RUTA ="http://192.168.0.43/api/"; //universidad
+    public static final String RUTA ="http://192.168.0.86/api/"; //universidad
     //public static final String RUTA ="http://192.168.0.17/api/";   //cuarto
 }
